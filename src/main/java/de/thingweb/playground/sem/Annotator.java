7ca634fbe3a8a4a7f9fb8f4c75bc273d45bb249c
Victor Charpenay <victor.charpenay@siemens.com>
2016-09-28 11:30:06 +0200

    fixed reasoner issue with OWL-Time (datatype not supported)

--- src/main/java/de/thingweb/playground/sem/Annotator.java
+++ src/main/java/de/thingweb/playground/sem/Annotator.java
@@ -12,11 +12,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import jdk.internal.dynalink.MonomorphicCallSite;
 
+import org.semanticweb.HermiT.Configuration;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.IRIDocumentSource;
+import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
 import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.AddImport;
 import org.semanticweb.owlapi.model.IRI;
@@ -33,6 +38,7 @@ import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyIRIMapper;
+import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.model.RemoveAxiom;
@@ -40,6 +46,8 @@ import org.semanticweb.owlapi.model.RemoveImport;
 import org.semanticweb.owlapi.reasoner.FreshEntityPolicy;
 import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 import org.semanticweb.owlapi.util.SimpleIRIMapper;
 
@@ -51,107 +59,108 @@ import com.hp.hpl.jena.rdf.model.StmtIterator;
 import de.thingweb.playground.sem.JenaAxiomFactory;
 
 public class Annotator {
-    
-    public static final String WOT_URI = "http://w3c.github.io/wot/wot.owl";
-    
-    private static final OWLOntologyManager sManager = OWLManager.createOWLOntologyManager();
-    private static final OWLDataFactory sFactory = sManager.getOWLDataFactory();
-    
-    private final OWLOntology WoT;
-    private final Set<String> mKnownOntologies;
-    
-    private OWLOntology mOntology;
-    private OWLReasoner mReasoner;
 
-    public Annotator(Map<String, String> m) throws OWLOntologyCreationException {
-	for (String k : m.keySet()) {
-	    OWLOntologyIRIMapper mapper = new SimpleIRIMapper(IRI.create(k),
-		    IRI.create(m.get(k)));
-	    sManager.addIRIMapper(mapper);
-	}
-	mKnownOntologies = m.keySet();
-	
-	mOntology = sManager.createOntology();
+  public static final String WOT_URI = "http://w3c.github.io/wot/wot.owl";
 
-	WoT = sManager.loadOntology(IRI.create(WOT_URI));
-	OWLImportsDeclaration wot = sFactory.getOWLImportsDeclaration(WoT.getOntologyID().getOntologyIRI());
-	sManager.applyChange(new AddImport(mOntology, wot));
-	
-	mReasoner = new Reasoner(mOntology);
+  private static final OWLOntologyManager sManager = OWLManager.createOWLOntologyManager();
+  private static final OWLDataFactory sFactory = sManager.getOWLDataFactory();
+
+  private final OWLOntology WoT;
+  private final Set<String> mKnownOntologies;
+
+  private OWLOntology mOntology;
+  private OWLReasoner mReasoner;
+
+  public Annotator(Map<String, String> m) throws OWLOntologyCreationException {
+    for (String k : m.keySet()) {
+      OWLOntologyIRIMapper mapper = new SimpleIRIMapper(IRI.create(k),
+                                                        IRI.create(m.get(k)));
+      sManager.addIRIMapper(mapper);
     }
-    
-    public void addThingDescription(InputStream stream, String format) throws IOException, AnnotatorException, OWLOntologyCreationException {
-	Model td = ModelFactory.createDefaultModel();
-	td.read(stream, "http://localhost/", format); // TODO parameterize base URL?
-	
-	JenaAxiomFactory jaf = new JenaAxiomFactory(sFactory);
-	List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
-	
-	for (OWLImportsDeclaration i : jaf.getImportDeclarations(td, mKnownOntologies)) {
-	    changes.add(new AddImport(mOntology, i));
-	    sManager.loadOntology(i.getIRI());
-	}
-	for (OWLAxiom a : jaf.getAxioms(td)) {
-	    changes.add(new AddAxiom(mOntology, a));
-	}
-	
-	sManager.applyChanges(changes);
-	if (!isConsistent()) {
-	    undoChanges(changes);
-	    throw new AnnotatorException(null); // FIXME how to find the affected axiom?
-	}
+    mKnownOntologies = m.keySet();
+
+    mOntology = sManager.createOntology();
+
+    WoT = sManager.loadOntology(IRI.create(WOT_URI));
+    OWLImportsDeclaration wot = sFactory.getOWLImportsDeclaration(WoT.getOntologyID().getOntologyIRI());
+    sManager.applyChange(new AddImport(mOntology, wot));
+  }
+
+  public void addThingDescription(InputStream stream, String format) throws IOException, AnnotatorException, OWLOntologyCreationException {
+    Model td = ModelFactory.createDefaultModel();
+    td.read(stream, "http://localhost/", format); // TODO parameterize base URL?
+
+    JenaAxiomFactory jaf = new JenaAxiomFactory(sFactory);
+    List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+
+    for (OWLImportsDeclaration i : jaf.getImportDeclarations(td, mKnownOntologies)) {
+      changes.add(new AddImport(mOntology, i));
+      sManager.loadOntology(i.getIRI());
     }
-    
-    public void addAnnotation(String entity, String type) throws AnnotatorException {
-	if (!mOntology.containsIndividualInSignature(IRI.create(entity))) {
-	    System.out.println("Note: annotated individual has not been defined yet."); // TODO throw exception
-	}
-	if (!mOntology.containsClassInSignature(IRI.create(type))) {
-	    System.out.println("Note: annotation type has not been defined yet"); // TODO throw exception
-	}
-	
-	List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
-	OWLClass t = sFactory.getOWLClass(IRI.create(type));
-	OWLNamedIndividual e = sFactory.getOWLNamedIndividual(IRI.create(entity));
-	OWLAxiom a = sFactory.getOWLClassAssertionAxiom(t, e);
-	changes.add(new AddAxiom(mOntology, a));
-	
-	sManager.applyChange(changes.get(0));
-	if (!isConsistent()) {
-	    undoChanges(changes);
-	    throw new AnnotatorException(a);
-	}
+    for (OWLAxiom a : jaf.getAxioms(td)) {
+      changes.add(new AddAxiom(mOntology, a));
     }
-    
-    public List<OWLClass> getSuggestions(String prefix) {
-	List<OWLClass> types = new ArrayList<OWLClass>();
-	
-	// TODO use index instead?
-	for (OWLClass t : mOntology.getClassesInSignature()) {
-	   if (t.getIRI().getFragment().startsWith(prefix)) {
-	       types.add(t);
-	   }
-	}
-	
-	return types;
+
+    sManager.applyChanges(changes);
+    if (!isConsistent()) {
+      undoChanges(changes);
+      throw new AnnotatorException(null); // FIXME how to find the affected axiom?
     }
-    
-    private boolean isConsistent() {
-	mReasoner = new Reasoner(mOntology);
-//	mReasoner.flush();
-	return mReasoner.isConsistent();
+  }
+
+  public void addAnnotation(String entity, String type) throws AnnotatorException {
+    if (!mOntology.containsIndividualInSignature(IRI.create(entity))) {
+      System.out.println("Note: annotated individual has not been defined yet."); // TODO throw exception
     }
-    
-    private void undoChanges(List<OWLOntologyChange> changes) {
-	List<OWLOntologyChange> inverseChanges = new ArrayList<OWLOntologyChange>();
-	for (OWLOntologyChange c : changes) {
-	    if (c.isAddAxiom()) {
-		inverseChanges.add(new RemoveAxiom(c.getOntology(), c.getAxiom()));
-	    } else if (c.isImportChange()) {
-		inverseChanges.add(new RemoveImport(c.getOntology(), ((AddImport) c).getImportDeclaration()));
-	    }
-	}
-	sManager.applyChanges(inverseChanges);
+    if (!mOntology.containsClassInSignature(IRI.create(type))) {
+      System.out.println("Note: annotation type has not been defined yet"); // TODO throw exception
     }
-    
+
+    List<OWLOntologyChange> changes = new ArrayList<OWLOntologyChange>();
+    OWLClass t = sFactory.getOWLClass(IRI.create(type));
+    OWLNamedIndividual e = sFactory.getOWLNamedIndividual(IRI.create(entity));
+    OWLAxiom a = sFactory.getOWLClassAssertionAxiom(t, e);
+    changes.add(new AddAxiom(mOntology, a));
+
+    sManager.applyChange(changes.get(0));
+    if (!isConsistent()) {
+      undoChanges(changes);
+      throw new AnnotatorException(a);
+    }
+  }
+
+  public List<OWLClass> getSuggestions(String prefix) {
+    List<OWLClass> types = new ArrayList<OWLClass>();
+
+    // TODO use index instead?
+    for (OWLClass t : mOntology.getClassesInSignature()) {
+      if (t.getIRI().getFragment().startsWith(prefix)) {
+        types.add(t);
+      }
+    }
+
+    return types;
+  }
+
+  private boolean isConsistent() {
+    // note: OWL-Time uses data types not supported by the reasoner
+    mReasoner = new Reasoner(new Configuration() {{
+      ignoreUnsupportedDatatypes = true;
+    }}, mOntology);
+    //	mReasoner.flush();
+    return mReasoner.isConsistent();
+  }
+
+  private void undoChanges(List<OWLOntologyChange> changes) {
+    List<OWLOntologyChange> inverseChanges = new ArrayList<OWLOntologyChange>();
+    for (OWLOntologyChange c : changes) {
+      if (c.isAddAxiom()) {
+        inverseChanges.add(new RemoveAxiom(c.getOntology(), c.getAxiom()));
+      } else if (c.isImportChange()) {
+        inverseChanges.add(new RemoveImport(c.getOntology(), ((AddImport) c).getImportDeclaration()));
+      }
+    }
+    sManager.applyChanges(inverseChanges);
+  }
+
 }
--- src/main/java/de/thingweb/playground/sem/JenaAxiomFactory.java
+++ src/main/java/de/thingweb/playground/sem/JenaAxiomFactory.java
@@ -33,80 +33,80 @@ import de.thingweb.playground.sem.Annotator;
  *
  */
 public class JenaAxiomFactory {
-    
-    private final OWLDataFactory mFactory;
-    private final OWLObjectProperty mRDFType;
-    
-    public JenaAxiomFactory(OWLDataFactory factory) {
-	mFactory = factory;
-	mRDFType = mFactory.getOWLObjectProperty(IRI.create("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"));
+
+  private final OWLDataFactory mFactory;
+  private final OWLObjectProperty mRDFType;
+
+  public JenaAxiomFactory(OWLDataFactory factory) {
+    mFactory = factory;
+    mRDFType = mFactory.getOWLObjectProperty(IRI.create("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"));
+  }
+
+  public Set<OWLAxiom> getAxioms(Model m) {
+    Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+
+    StmtIterator it = m.listStatements();
+    while (it.hasNext()) {
+      Statement stm = it.next();
+      OWLIndividual s = (stm.getSubject().isAnon()) ?
+         mFactory.getOWLAnonymousIndividual() :
+         mFactory.getOWLNamedIndividual(IRI.create(stm.getSubject().getURI()));
+       if (stm.getObject().isResource()) {
+         OWLObjectProperty p = mFactory.getOWLObjectProperty(IRI.create(stm.getPredicate().getURI()));
+         if (p.equals(mRDFType)) {
+           OWLClass t = mFactory.getOWLClass(IRI.create(stm.getObject().asResource().getURI()));
+           axioms.add(mFactory.getOWLClassAssertionAxiom(t, s));
+         } else {
+           OWLIndividual o = (stm.getObject().isAnon()) ?
+                                                         mFactory.getOWLAnonymousIndividual() :
+                                                           mFactory.getOWLNamedIndividual(IRI.create(stm.getObject().asResource().getURI()));
+                                                         axioms.add(mFactory.getOWLObjectPropertyAssertionAxiom(p, s, o));
+         }
+       } else {
+         OWLDataProperty p = mFactory.getOWLDataProperty(IRI.create(stm.getPredicate().getURI()));
+         axioms.add(mFactory.getOWLDataPropertyAssertionAxiom(p, s, getOWLLiteral(stm.getObject().asLiteral())));
+       }
     }
 
-    public Set<OWLAxiom> getAxioms(Model m) {
-	Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+    return axioms;
+  }
 
-	StmtIterator it = m.listStatements();
-	while (it.hasNext()) {
-	    Statement stm = it.next();
-        	OWLIndividual s = (stm.getSubject().isAnon()) ?
-        		    mFactory.getOWLAnonymousIndividual() :
-        		    mFactory.getOWLNamedIndividual(IRI.create(stm.getSubject().getURI()));
-        	if (stm.getObject().isResource()) {
-        	    OWLObjectProperty p = mFactory.getOWLObjectProperty(IRI.create(stm.getPredicate().getURI()));
-        	    if (p.equals(mRDFType)) {
-        		OWLClass t = mFactory.getOWLClass(IRI.create(stm.getObject().asResource().getURI()));
-        		axioms.add(mFactory.getOWLClassAssertionAxiom(t, s));
-        	    } else {
-        		OWLIndividual o = (stm.getObject().isAnon()) ?
-        			mFactory.getOWLAnonymousIndividual() :
-        			mFactory.getOWLNamedIndividual(IRI.create(stm.getObject().asResource().getURI()));
-        		axioms.add(mFactory.getOWLObjectPropertyAssertionAxiom(p, s, o));
-        	    }
-        	} else {
-        	    OWLDataProperty p = mFactory.getOWLDataProperty(IRI.create(stm.getPredicate().getURI()));
-        	    axioms.add(mFactory.getOWLDataPropertyAssertionAxiom(p, s, getOWLLiteral(stm.getObject().asLiteral())));
-        	}
-	}
-	    
-	return axioms;
+  public Set<OWLImportsDeclaration> getImportDeclarations(Model m, Set<String> ontologies) {
+    Set<OWLImportsDeclaration> decl = new HashSet<OWLImportsDeclaration>();
+
+    StmtIterator stmIt = m.listStatements();
+    while (stmIt.hasNext()) {
+      Statement stm = stmIt.next();
+      String p = toOntoURI(stm.getPredicate().getNameSpace());
+      if (!p.equals(Annotator.WOT_URI) && ontologies.contains(p)) {
+        decl.add(mFactory.getOWLImportsDeclaration(IRI.create(p)));
+      }
+      if (stm.getObject().isURIResource()) {
+        String o = toOntoURI(stm.getObject().asResource().getNameSpace());
+        if (!o.equals(Annotator.WOT_URI) && ontologies.contains(o)) {
+          decl.add(mFactory.getOWLImportsDeclaration(IRI.create(o)));
+        }
+      }
     }
-    
-    public Set<OWLImportsDeclaration> getImportDeclarations(Model m, Set<String> ontologies) {
-	Set<OWLImportsDeclaration> decl = new HashSet<OWLImportsDeclaration>();
-	
-	StmtIterator stmIt = m.listStatements();
-	while (stmIt.hasNext()) {
-	    Statement stm = stmIt.next();
-	    String p = toOntoURI(stm.getPredicate().getNameSpace());
-	    if (!p.equals(Annotator.WOT_URI) && ontologies.contains(p)) {
-		decl.add(mFactory.getOWLImportsDeclaration(IRI.create(p)));
-	    }
-	    if (stm.getObject().isURIResource()) {
-		String o = toOntoURI(stm.getObject().asResource().getNameSpace());
-		if (!o.equals(Annotator.WOT_URI) && ontologies.contains(o)) {
-			decl.add(mFactory.getOWLImportsDeclaration(IRI.create(o)));
-		}
-	    }
-	}
-	
-	return decl;
+
+    return decl;
+  }
+
+  private OWLLiteral getOWLLiteral(Literal l) {
+    if (l.getDatatype() == null) {
+      // assumed string
+      return mFactory.getOWLLiteral(l.getLexicalForm());
+    } else {
+      OWLDatatype type = mFactory.getOWLDatatype(IRI.create(l.getDatatypeURI()));
+      return mFactory.getOWLLiteral(l.getLexicalForm(), type);
     }
-    
-    private OWLLiteral getOWLLiteral(Literal l) {
-	if (l.getDatatype() == null) {
-	    // assumed string
-	    return mFactory.getOWLLiteral(l.getLexicalForm());
-	} else {
-	    OWLDatatype type = mFactory.getOWLDatatype(IRI.create(l.getDatatypeURI()));
-	    return mFactory.getOWLLiteral(l.getLexicalForm(), type);
-	}
+  }
+
+  private String toOntoURI(String namespace) {
+    if (namespace.endsWith("#") || namespace.endsWith("/")) {
+      return namespace.substring(0, namespace.length() - 1); // remove last char
     }
-    
-    private String toOntoURI(String namespace) {
-	if (namespace.endsWith("#") || namespace.endsWith("/")) {
-	    return namespace.substring(0, namespace.length() - 1); // remove last char
-	}
-	return namespace;
-    }
-    
+    return namespace;
+  }
+
 }
