293ab69fe43ac5681dc6a14218f7c6b6142face6
Victor Charpenay <victor.charpenay@siemens.com>
2016-09-28 16:38:14 +0200

    fixed Gradle build file (now generates proper .war file), replaced annotations with defs in web.xml.

--- src/main/java/de/thingweb/playground/servlet/AnnotatorServlet.java
+++ src/main/java/de/thingweb/playground/servlet/AnnotatorServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,9 +14,8 @@ import de.thingweb.playground.sem.Annotator;
 import de.thingweb.playground.sem.AnnotatorException;
 
 /**
- * Servlet implementation class AnnotatorServlet
+ * Semantic validation of a Thing Description
  */
-@WebServlet(name = "sem", description = "Semantic validation of a Thing Description", urlPatterns = { "/sem" })
 public class AnnotatorServlet extends HttpServlet {
   
     private static final long serialVersionUID = 1L;
