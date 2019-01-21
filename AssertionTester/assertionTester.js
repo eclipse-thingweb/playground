a42f139ca258f11b8b9c14a532cf73bb4fe4be91
Ege Korkan <egekorkan@gmail.com>
2019-01-21 09:38:00 +0100

    add assertions

--- AssertionTester/assertionTester.js
+++ AssertionTester/assertionTester.js
@@ -252,7 +252,7 @@ function checkVocabulary(tdJson) {
     ajv.addSchema(schema, 'td');
 
     var valid = ajv.validate('td', tdJson);
-    var otherAssertions = ["td-objects:securityDefinitions", "td-arrays:security", "td:security", "td-security-mandatory"];
+    var otherAssertions = ["td-objects:securityDefinitions", "td-arrays:security", "td:security", "td-security-mandatory", "td-vocab-securityDefinitions", "td-vocab-security"];
 
     if (valid) {
         results.push({
