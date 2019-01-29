e18bc603acd82b5c0ee86472a3f0b55c01d75cf6
Ege Korkan <egekorkan@gmail.com>
2019-01-30 00:21:03 +0100

    add parent assertions after merge

--- AssertionTester/mergeResults.js
+++ AssertionTester/mergeResults.js
@@ -82,6 +82,7 @@ function merge_results(results, done_callback) {
                 cm = "";
             }
             let current = merged_results.get(id);
+
             if (undefined === current) {
                 merged_results.set(id, [st, cm]);
             } else {
@@ -94,7 +95,7 @@ function merge_results(results, done_callback) {
                 } else if ("pass" === st || "pass" === current_st) {
                     // pass dominates null, since someone could have tested it somewhere
                     merged_results.set(id, ["pass", get_comment(st, current_st, "pass", cm, current_cm)]);
-                
+
                 } else if (("null" === st && "null" === current_st)) {
                     // null dominates not impl
                     merged_results.set(id, ["null", get_comment(st, current_st, "null", cm, current_cm)]);
@@ -104,6 +105,60 @@ function merge_results(results, done_callback) {
                 }
             }
         }
+    }
+    //iterate through to find the parent results
+    var iterator1 = merged_results[Symbol.iterator]();
+
+    var parentsJson = {};
+
+    for (let item of iterator1) {
+        //find child, then
+        //find parent
+        // if child can overwrite parent, do that, otherwise keep going
+        var curId = item[0];
+        var underScoreLoc = curId.indexOf('_');
+        if (underScoreLoc === -1) {
+            //this is a parent or an assertion with no child so we dont care
+        }
+        else {
+            // this is a child assertion
+            var childResult = item[1][0];
+            var parentID = curId.slice(0, underScoreLoc);
+            if (parentsJson.hasOwnProperty(parentID)) {
+                parentsJson[parentID].push(childResult);
+            } else {
+                parentsJson[parentID] = [];
+                parentsJson[parentID].push(childResult);
+            }
+        }
+        
+        parentsJsonArray = Object.getOwnPropertyNames(parentsJson);
+        parentsJsonArray.forEach((curParentId, indexParent) => {
+
+            var curParent = parentsJson[curParentId];
+            
+            for (let index = 0; index < curParent.length; index++) {
+                const curChild = curParent[index];
+                if (curChild.Status == "fail") {
+                    //push fail and break, i.e stop going through children, we are done here!
+                    merged_results.set(curParentId, ["fail", "a child is failed"]);
+                    break;
+                } else if (curChild.Status == "not-impl") {
+                    //push not-impl and break, i.e stop going through children, we are done here!
+                    merged_results.set(curParentId, ["not-impl", "a child is not implemented"]);
+                    break;
+                } else if (curChild.Status == "null"){
+                    merged_results.set(curParentId, ["null", "a child is not tested"]);
+                    break;
+                } else {
+                    // if reached the end without break, push pass
+                    if (index == curParent.length - 1) {
+                        merged_results.set(curParentId, ["pass", "all childs passed"]);
+                    }
+                }
+            }
+        });
+        
     }
     done_callback(merged_results);
 }
@@ -129,80 +184,6 @@ function get_comment(
     return comment;
 }
 
-function createParents(resultsJSON,merged_results) {
-
-    //create a json object with parent name keys and then each of them an array of children results
-
-    var parentsJson = {};
-    resultsJSON.forEach((curResult, index) => {
-        var curId = curResult.ID;
-        var underScoreLoc = curId.indexOf('_');
-        if (underScoreLoc === -1) {
-            // this assertion is not a child assertion
-        } else {
-            var parentResultID = curId.slice(0, underScoreLoc);
-            // if it already exists push otherwise create an array and push
-            if (parentsJson.hasOwnProperty(parentResultID)) {
-                parentsJson[parentResultID].push(curResult);
-            } else {
-                parentsJson[parentResultID] = [];
-                parentsJson[parentResultID].push(curResult);
-            }
-            // console.log(parentsJson);
-        }
-    });
-
-    //Go through the object and push a result that is an OR of each children
-    // if one children is fail, result is fail
-    // if one children is not-impl, result is not-impl
-    // if none of these happen, then it implies it is pass, so result is pass
-    // "ID": schema.title,
-    // "Status": "not-impl" 
-
-    parentsJsonArray = Object.getOwnPropertyNames(parentsJson);
-    parentsJsonArray.forEach((curParentName, indexParent) => {
-
-        var curParent = parentsJson[curParentName];
-
-        for (let index = 0; index < curParent.length; index++) {
-            const curChild = curParent[index];
-            let current = merged_results.get(curParent);
-            if (curChild.Status == "fail") {
-                //push fail and break, i.e stop going through children, we are done here!
-                // json_results
-                
-                
-                merged_results.set(current, ["fail"]);
-                // results.push({
-                //     "ID": curParentName,
-                //     "Status": "fail",
-                //     "additionalInfo": "Error message can be seen in the children assertions"
-                // });
-                break;
-            } else if (curChild.Status == "not-impl") {
-                //push not-impl and break, i.e stop going through children, we are done here!
-                merged_results.set(current, ["not-impl"]);
-                // results.push({
-                //     "ID": curParentName,
-                //     "Status": "not-impl",
-                //     "additionalInfo": "Error message can be seen in the children assertions"
-                // });
-                break;
-            } else {
-                // if reached the end without break, push pass
-                if (index == curParent.length - 1) {
-                    merged_results.set(current, ["pass"]);
-                    // results.push({
-                    //     "ID": curParentName,
-                    //     "Status": "pass",
-                    // });
-                }
-            }
-        }
-    });
-
-}
-
 function output_results(merged_results) {
     process.stdout.write('"ID","Status","Comment"\n');
     // console.log(merged_results);
@@ -210,32 +191,21 @@ function output_results(merged_results) {
     merged_results.forEach((data, id) => {
 
         process.stdout.write('"' + id + '","' + data[0] + '",\n');
-       
+
         json_results.push({
             "ID": id,
-            "Status": data[0]
+            "Status": data[0],
+            "additionalInfo":data[1]
         });
     });
-    // console.log(json_results);
-    // process.exit();
-    // var newRes=[];
-    // createParents(json_results,merge_results);
-    //     merged_results.forEach((data, id) => {
 
-    //         process.stdout.write('"' + id + '","' + data[0] + '",\n');
-    //         // console.log("id is ",id," data is", data[0])
-    //         newRes.push({
-    //             "ID": id,
-    //             "Status": data[0]
-    //         });
-    //     });
     var csvResults = json2csvParser.parse(json_results);
     fs.writeFile("./Results/mergedResults.csv", csvResults, function (err) {
         if (err) {
             return console.log(err);
         }
 
-        console.log("The merged result csv was saved!");
+        console.warn("The merged result csv was saved!");
         process.exit(0);
     });
 }
@@ -281,7 +251,7 @@ if (process.argv.length > 3) {
             output_results(merged_results);
             // Success
         });
-        
+
     });
 
 } else {
