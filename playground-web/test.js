33ab50b88a0de8681a6c00d742dba1b2c299359d
Adrian Kast <adrian.kast@tum.de>
2020-09-09 17:25:35 +0200

    improve playwright test procedure

--- playground-web/test.js
+++ playground-web/test.js
@@ -13,28 +13,124 @@
  * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
  ********************************************************************************/
 
-const playwright = require('playwright');
-const fs = require("fs");
- 
-(async () => {
-  for (const browserType of ['chromium', 'firefox'/*, 'webkit'*/]) {
+const playwright = require('playwright')
+const fs = require("fs")
+const handler = require("serve-handler")
+const http = require("http")
+
+const server = http.createServer((request, response) => {
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/vercel/serve-handler#options
+  return handler(request, response);
+})
+
+server.listen(3000, () => {
+  console.log('Running siteTest at http://localhost:3000');
+});
+
+testVisual()
+
+async function testVisual() {
+  if (!fs.existsSync("./test_results")) {fs.mkdirSync("./test_results")}
+
+
+  const BrowserList = []
+  BrowserList.push("chromium")
+  BrowserList.push("firefox")
+  // BrowserList.push("webkit")
+
+  for (const browserType of BrowserList) {
+    // use {headless: false, slowMo: 200} options in launch() for debugging
     const browser = await playwright[browserType].launch()
-    const context = await browser.newContext()
+    const context = await browser.newContext({acceptDownloads: true})
     const page = await context.newPage()
 
-    if (!fs.existsSync("./test_results")) {fs.mkdirSync("./test_results")}
-
-    await page.goto('http://plugfest.thingweb.io/playground/', {waitUntil: "networkidle"})
-    await page.screenshot({ path: `./test_results/index-of-${browserType}.png` })
-
-    await page.click('#load_example', {waitUntil: "networkidle"})
-    await page.screenshot({ path: `./test_results/dropdown-of-${browserType}.png` })
-
-    await page.selectOption('#load_example', "SimpleTD")
-    // await page.dispatchEvent('#load_example', "change")
-    await page.screenshot({ path: `./test_results/td-of-${browserType}.png` })
+    if (browserType === "chromium") {
+      await testVisualChromium(page)
+    }
+    else if (browserType === "firefox") {
+      await testVisualFirefox(page)
+    }
+    else if (browserType === "webkit") {
+      await testVisualWebkit(page)
+    }
 
     await browser.close()
     console.log("Finished testing with: " + browserType)
   }
-})()
+
+  server.close()
+}
+
+async function testVisualChromium(page) {
+  const browserType = "chromium"
+  await page.goto('http://localhost:3000')
+  await page.screenshot({ path: `./test_results/index-of-${browserType}.png` })
+
+  await page.click('#load_example', {waitUntil: "networkidle"})
+  await page.screenshot({ path: `./test_results/chromium_dropdown-examples.png` })
+
+  await page.selectOption('#load_example', "SimpleTD")
+  await page.screenshot({ path: `./test_results/chromium_td.png` })
+
+  await page.click("#btn_validate")
+  await myWait(1000)
+  await page.screenshot({ path: `./test_results/chromium_validation.png`, fullPage: true })
+
+  await page.click("#validation_table_head")
+  await page.screenshot({ path: `./test_results/chromium_lights.png` })
+
+  await page.click("#btn_assertion_popup")
+  await page.screenshot({ path: `./test_results/chromium_assertion-popup.png` })
+  const [assertionDownload] = await Promise.all([
+    page.waitForEvent("download"),
+    page.click("#btn_assertion")
+  ])
+  assertionDownload.saveAs("./test_results/assertions.csv")
+  await page.click("#close_assertion_test_popup")
+
+  await page.click("#btn_clearLog")
+  const consoleField = await page.$("#console")
+  await consoleField.screenshot({ path: `./test_results/chromium_cleared-log.png` })
+
+  await page.click("#editor_theme")
+  await page.screenshot({ path: `./test_results/chromium_dropdown-editor-color.png` })
+
+  await page.selectOption('#editor_theme', "vs-dark")
+  await page.screenshot({ path: `./test_results/chromium_dark-editor.png` })
+}
+
+async function testVisualFirefox(page) {
+  const browserType = "firefox"
+  await page.goto('http://localhost:3000', {waitUntil: "networkidle"})
+  await myWait(1000)
+  await page.screenshot({ path: `./test_results/index-of-${browserType}.png` })
+
+  // await page.click('#load_example', {waitUntil: "networkidle"})
+  // await myWait()
+  // const elementHandle = await page.$('.loadExampleForm');
+  // await elementHandle.screenshot({path: "./test_results/only-dropdown.png"});
+  // await page.screenshot({ path: `./test_results/dropdown-of-${browserType}.png`, fullPage: true })
+
+  // await page.selectOption('#load_example', "SimpleTD")
+  // await page.screenshot({ path: `./test_results/td-of-${browserType}.png` })
+}
+
+async function testVisualWebkit(page) {
+  const browserType = "webkit"
+  page.on("console", msg => console.log(msg.text()))
+  await page.goto('http://localhost:3000', {waitUntil: "networkidle"})
+  await page.screenshot({ path: `./test_results/index-of-${browserType}.png` })
+}
+
+/**
+ * helper function that resolves after given time
+ * @param {number} milliseconds time to wait in milliseconds
+ */
+function myWait(milliseconds){
+  return new Promise( (res, rej) => {
+    setTimeout( () => {
+      res()
+    }, milliseconds)
+  })
+}
