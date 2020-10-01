deba8dbbb9d2c37ea966c91b66f11ea6d826612f
Adrian Kast <adrian.kast@tum.de>
2020-10-01 15:00:54 +0200

    prepare production

--- playground-td_to_openAPI/testAll.js
+++ playground-td_to_openAPI/testAll.js
@@ -1,3 +1,18 @@
+/* *******************************************************************************
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ * Document License (2015-05-13) which is available at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+
 const fs = require("fs")
 const toOAP = require("./index.js")
 
@@ -16,10 +31,10 @@ filenames.forEach( filename => {
         fs.writeFileSync("./out/" + filename.slice(0, -5) + "_openapi.json", JSON.stringify(apiSpec.json, undefined, 2))
         // console.log(JSON.stringify(apiSpec, undefined, 4))
         // console.log("VALID")
-        valid++;
+        valid++
         console.log("valid: " + valid + "/" + (fail + valid) + " (total: " + all + ")")
     }, err => {
-        fail++;
+        fail++
         console.error("Problem in " + filename)
         console.error(err.message)
     })
