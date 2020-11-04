d7dde776b93737b1aa0fe46d98c1d4b6b2b335b9
Adrian Kast <adrian.kast@tum.de>
2020-11-04 14:49:41 +0100

    start implementation

--- packages/playground-add_defaults/index.js
+++ packages/playground-add_defaults/index.js
@@ -9,7 +9,33 @@ function addDefaults(td) {
 
     // find objects to extend
 
-    // extend properly
+    // -- Action Affordances
+    if (td.actions) {
+        Object.keys(td.actions).forEach( actionName => {
+            extendOneObject(td.actions[actionName], defaultClasses.actionAffordance)
+        })
+    }
+
+    // -- Forms
+    const interactionTypes = {
+        properties: defaultClasses.formPropertyAffordance, 
+        actions: defaultClasses.formActionAffordance, 
+        events: defaultClasses.formEventAffordance
+    }
+    Object.keys(interactionTypes).forEach( interactionType => {
+        if (td[interactionType]) {
+            Object.keys(td[interactionType]).forEach( interactionName => {
+                const interaction = td[interactionType][interactionName]
+                if (interaction.forms) {
+                    interaction.forms.forEach( form => {
+                        extendOneObject(form, interactionTypes[interactionType])
+                    })
+                }
+            })
+        }
+    })
+
+
 
     return td
 }
@@ -19,48 +45,82 @@ function addDefaults(td) {
  * https://www.w3.org/TR/2020/REC-wot-thing-description-20200409/
  * 
  * represented with:
- * property -> ...Classes -> value
+ * class[-superClass] -> property -> value
  */
 const defaultLookup = {
-    contentType: {
-        Form: "application/json"
+    Form: {
+        contentType: "application/json",
     },
-    readOnly: {
-        DataSchema: false
+    "Form-PropertyAffordance": {
+        op: ["readproperty", "writeproperty"]
     },
-    writeOnly: {
-        DataSchema: false
+    "Form-ActionAffordance": {
+        op: "invokeaction"
     },
-    safe: {
-        ActionAffordance: false
+    "Form-EventAffordance": {
+        op: "subscribeevent" // will be ["subscribeevent", "unsubscribeevent"] in the future
     },
-    idempotent: {
-        ActionAffordance: false
+    DataSchema: {
+        readOnly: false,
+        writeOnly: false
     },
-    op: {
-        PropertyAffordance: {
-            Form: ["readproperty", "writeproperty"]
-        },
-        ActionAffordance: {
-            Form: "invokeaction"
-        },
-        EventAffordance: {
-            Form: "subscribeevent" // will be ["subscribeevent", "unsubscribeevent"] in the future
+    ActionAffordance: {
+        safe: false,
+        idempotent: false
+    },
+    BasicSecuritySchema: {
+        in: "header"
+    },
+    DigestSecurityScheme: {
+        in: "header",
+        qop: "auth"
+    },
+    BearerSecurityScheme: {
+        in: "header",
+        alg: "ES256",
+        format: "jwt"
+    },
+    APIKeySecurityScheme: {
+        in: "query"
+    }
+}
+
+/**
+ * The possible types of objects to extend
+ */
+const defaultClasses = {
+    formPropertyAffordance: "Form-PropertyAffordance",
+    formActionAffordance: "Form-ActionAffordance",
+    formEventAffordance: "Form-EventAffordance",
+    dataSchema: "DataSchema",
+    actionAffordance: "ActionAffordance",
+    basicSecuritySchema: "BasicSecuritySchema",
+    digestSecurityScheme: "DigestSecurityScheme",
+    bearerSecurityScheme: "BearerSecurityScheme",
+    apiKeySecurityScheme: "APIKeySecurityScheme"
+}
+
+/**
+ * Adds default values to one given object,
+ * using the defaultLookup table
+ * @param {{}} target the object to extend
+ * @param {string} type The type according to the defaultLookup table
+ */
+function extendOneObject(target, type) {
+    if (typeof target !== "object") {throw new Error("target has to be of type 'object' not: " + typeof target)}
+    if (typeof type !== "string") {throw new Error("type has to be of type 'string' not: " + typeof type)}
+    if (Object.keys(defaultLookup).every(key => (key !== type))) {throw new Error("type has to be a defaultLookup entry, type: " + type)}
+
+    const defaultSource = defaultLookup[type]
+    Object.keys(defaultSource).forEach( key => {
+        if (target[key] === undefined) {
+            target[key] = defaultSource[key]
         }
-    },
-    in: {
-        BasicSecuritySchema: "header",
-        DigestSecurityScheme: "header",
-        BearerSecurityScheme: "header",
-        APIKeySecurityScheme: "query"
-    },
-    qop: {
-        DigestSecurityScheme: "auth"
-    },
-    alg: {
-        BearerSecurityScheme: "ES256"
-    },
-    format: {
-        BearerSecurityScheme: "jwt"
+    })
+
+    // handle default values with superClasses (e.g. Form-EventAffordance)
+    if (type.includes("-")) {
+        const superType = type.split("-").slice(0,-1).join("-")
+        extendOneObject(target, superType)
     }
 }
