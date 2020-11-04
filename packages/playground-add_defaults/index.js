a31a956a4a3c83eaf611a86a6ff8ed16f3edc4fe
Adrian Kast <adrian.kast@tum.de>
2020-11-04 11:30:42 +0100

    add default table

--- packages/playground-add_defaults/index.js
+++ packages/playground-add_defaults/index.js
@@ -7,7 +7,60 @@ module.exports = addDefaults
 function addDefaults(td) {
     if (typeof td === "string") {td = JSON.parse(td)}
 
-    
+    // find objects to extend
+
+    // extend properly
 
     return td
+}
+
+/**
+ * the TD default value definitions according to 
+ * https://www.w3.org/TR/2020/REC-wot-thing-description-20200409/
+ * 
+ * represented with:
+ * property -> ...Classes -> value
+ */
+const defaultLookup = {
+    contentType: {
+        Form: "application/json"
+    },
+    readOnly: {
+        DataSchema: false
+    },
+    writeOnly: {
+        DataSchema: false
+    },
+    safe: {
+        ActionAffordance: false
+    },
+    idempotent: {
+        ActionAffordance: false
+    },
+    op: {
+        PropertyAffordance: {
+            Form: ["readproperty", "writeproperty"]
+        },
+        ActionAffordance: {
+            Form: "invokeaction"
+        },
+        EventAffordance: {
+            Form: "subscribeevent" // will be ["subscribeevent", "unsubscribeevent"] in the future
+        }
+    },
+    in: {
+        BasicSecuritySchema: "header",
+        DigestSecurityScheme: "header",
+        BearerSecurityScheme: "header",
+        APIKeySecurityScheme: "query"
+    },
+    qop: {
+        DigestSecurityScheme: "auth"
+    },
+    alg: {
+        BearerSecurityScheme: "ES256"
+    },
+    format: {
+        BearerSecurityScheme: "jwt"
+    }
 }
