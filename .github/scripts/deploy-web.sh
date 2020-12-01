5e4b733f4bf5d7cfb3098264a6ea834d21ccfbb6
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-01 14:04:19 +0100

    try own ssh test server

--- .github/scripts/deploy-web.sh
+++ .github/scripts/deploy-web.sh
@@ -1,3 +1,3 @@
-ssh bandit0@bandit.labs.overthewire.org -p 2220
-bandit0
-ls
+ASDF_TEST="$(mktemp)"
+echo "$PG_TEST" > $ASDF_TEST
+ssh -i $ASDF_TEST -o StrictHostKeyChecking=no $MY_HOST "ls; ls -la"
