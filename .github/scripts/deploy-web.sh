30473418c696dff438a0d1ace6d856b8aef6c35a
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-01 15:22:53 +0100

    final?

--- .github/scripts/deploy-web.sh
+++ .github/scripts/deploy-web.sh
@@ -1,3 +1,4 @@
 ASDF_TEST="$(mktemp)"
 echo "$PG_TEST" > $ASDF_TEST
-ssh -i $ASDF_TEST -o StrictHostKeyChecking=no $MY_HOST "git clone https://github.com/$GITHUB_REPOSITORY.git; cd thingweb-playground; lerna bootstrap; cp -r ./packages/playground-web/ /var/www/html/; cd ../; rm -r -f thingweb-playground; echo cd done"
+ssh -i $ASDF_TEST -o StrictHostKeyChecking=no $MY_HOST "git clone https://github.com/$GITHUB_REPOSITORY.git; cd thingweb-playground; lerna bootstrap; cd ./packages/playground-web; npm install; rm -r -f /var/www/html/*; cp -r ./* /var/www/html; cd ../../../; rm -r -f -v thingweb-playground"
+echo "cd done"
