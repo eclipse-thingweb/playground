145e3b2d0c5c96f9a8cc1b45839ba2167749280e
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-01 14:18:27 +0100

    try ssh deployment

--- .github/scripts/deploy-web.sh
+++ .github/scripts/deploy-web.sh
@@ -1,3 +1,3 @@
 ASDF_TEST="$(mktemp)"
 echo "$PG_TEST" > $ASDF_TEST
-ssh -i $ASDF_TEST -o StrictHostKeyChecking=no $MY_HOST "ls; ls -la"
+ssh -i $ASDF_TEST -o StrictHostKeyChecking=no $MY_HOST "git clone git@github.com:$GITHUB_REPOSITORY.git; cd thingweb-playground; lerna bootstrap; cp -r ./packages/web/ /var/www/html/; cd ../; rm -r thingweb-playground"
