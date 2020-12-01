b252cf32581ca3135aae1356907ca7251092e827
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-01 15:55:10 +0100

    test if fails

--- .github/scripts/deploy-web.sh
+++ .github/scripts/deploy-web.sh
@@ -1,4 +1,3 @@
 TMP_PASS_FILE="$(mktemp)"
 echo "$SSH_PASS" > $TMP_PASS_FILE
-ssh -i $TMP_PASS_FILE -o StrictHostKeyChecking=no $SSH_HOST "git clone https://github.com/$GITHUB_REPOSITORY.git; cd thingweb-playground; lerna bootstrap; cd ./packages/playground-web; npm install; rm -r -f /var/www/html/*; cp -r ./* /var/www/html; cd ../../../; rm -r -f thingweb-playground"
-echo "cd done"
+ssh -i $TMP_PASS_FILE -o StrictHostKeyChecking=no $SSH_HOST "git clone https://github.com/$GITHUB_REPOSITORY.git; cd thingweb-playground; lerna bootstrap; cd ./packages/playground-web; npm install; rm -r -f /var/www/html/*; cp -r ./* /var/www/html; cd ../../../; rm -r -f thingweb-playground; echo CD DONE"
