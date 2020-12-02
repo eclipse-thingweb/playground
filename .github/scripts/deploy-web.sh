561ecf6ae96be8b0947c78a481154352984fdce7
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-02 10:35:44 +0100

    replace not needed env var with hardcoded path

--- .github/scripts/deploy-web.sh
+++ .github/scripts/deploy-web.sh
@@ -1,3 +1,3 @@
 TMP_PASS_FILE="$(mktemp)"
 echo "$SSH_PASS" > $TMP_PASS_FILE
-ssh -i $TMP_PASS_FILE -o StrictHostKeyChecking=no -o LogLevel=error $SSH_HOST "git clone https://github.com/$GITHUB_REPOSITORY.git; cd thingweb-playground; lerna bootstrap; cd ./packages/web; npm install; echo installed locally; rm -r -f /var/www/html/*; cp -r ./* /var/www/html; echo cleaned and copy to webdir; cd ../../../; rm -r -f thingweb-playground; echo CD DONE"
+ssh -i $TMP_PASS_FILE -o StrictHostKeyChecking=no -o LogLevel=error $SSH_HOST "git clone https://github.com/thingweb/thingweb-playground.git; cd thingweb-playground; lerna bootstrap; cd ./packages/web; npm install; echo installed locally; rm -r -f /var/www/html/*; cp -r ./* /var/www/html; echo cleaned and copy to webdir; cd ../../../; rm -r -f thingweb-playground; echo CD DONE"
