ec399f68a214612101b7ffc88259925e63e4c524
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-04 12:25:14 +0100

    fix typo

--- packages/gist_backend/config.js
+++ packages/gist_backend/config.js
@@ -6,9 +6,9 @@ const port = availablePorts.production
 const keepLogMessages = 100
 const allowHosts = [
     "http://127.0.0.1:5500",
-    "http://localhost:5500" 
-    "http://127.0.0.1", 
-    "http://localhost", 
+    "http://localhost:5500",
+    "http://127.0.0.1",
+    "http://localhost",
     "http://plugfest.thingweb.io"
 ]
 const login = process.env.TD_PLAYGROUND_LOGIN
