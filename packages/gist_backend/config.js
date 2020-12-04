2f4567a69cda7b87eab3c6ac8b720ce058111564
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-04 12:16:24 +0100

    test other port

--- packages/gist_backend/config.js
+++ packages/gist_backend/config.js
@@ -1,4 +1,8 @@
-const port = 8082
+const availablePorts = {
+    production: 8082,
+    test: 3000
+}
+const port = availablePorts.test
 const keepLogMessages = 100
 const allowHosts = ["http://127.0.0.1:5500", "http://localhost:5500", "http://plugfest.thingweb.io"]
 const login = process.env.TD_PLAYGROUND_LOGIN
