ea85dfcdbcfee0c001802ea778262fb9008b2a18
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2020-12-04 12:24:05 +0100

    try adapting CORS

--- packages/gist_backend/config.js
+++ packages/gist_backend/config.js
@@ -1,10 +1,16 @@
 const availablePorts = {
     production: 8082,
-    test: 3000
+    test: 3030
 }
-const port = availablePorts.test
+const port = availablePorts.production
 const keepLogMessages = 100
-const allowHosts = ["http://127.0.0.1:5500", "http://localhost:5500", "http://plugfest.thingweb.io"]
+const allowHosts = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500" 
+    "http://127.0.0.1", 
+    "http://localhost", 
+    "http://plugfest.thingweb.io"
+]
 const login = process.env.TD_PLAYGROUND_LOGIN
 const token = process.env.TD_PLAYGROUND_TOKEN
 
