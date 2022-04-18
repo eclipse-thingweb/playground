47250a441eeb5c377bb8975ba93218860e6685a5
Ege Korkan <egekorkan@gmail.com>
2022-04-18 14:18:00 +0200

    Fix CD pipeline script to include symlinks

--- .github/scripts/deploy-web.sh
+++ .github/scripts/deploy-web.sh
@@ -1,3 +1,3 @@
 TMP_PASS_FILE="$(mktemp)"
 echo "$SSH_PASS" > $TMP_PASS_FILE
-ssh -i $TMP_PASS_FILE -o StrictHostKeyChecking=no -o LogLevel=error $SSH_HOST "rm -r -f thingweb-playground; git clone https://github.com/thingweb/thingweb-playground.git; cd thingweb-playground; lerna bootstrap --no-ci; cd ./packages/web; npm install; echo installed locally; rm -r -f /var/www/html/playground/*; cp -r ./* /var/www/html/playground; echo cleaned and copy to webdir; echo CD DONE"
+ssh -i $TMP_PASS_FILE -o StrictHostKeyChecking=no -o LogLevel=error $SSH_HOST "rm -r -f thingweb-playground; git clone https://github.com/thingweb/thingweb-playground.git; cd thingweb-playground; lerna bootstrap --no-ci; cd ./packages/web; npm install; echo installed locally; rm -r -f /var/www/html/playground/*; cp -L -r ./* /var/www/html/playground; echo cleaned and copy to webdir; echo CD DONE"
