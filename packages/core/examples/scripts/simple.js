6e7f9cd81ee27d9c78df07d4c93a70aa4c1cdf54
Ege Korkan <egekorkan@gmail.com>
2022-04-27 11:50:33 +0200

    cleaning test

--- packages/core/examples/scripts/simple.js
+++ packages/core/examples/scripts/simple.js
@@ -2,51 +2,52 @@
 const tdValidator = require("../../index").tdValidator
 
 const simpleTD = JSON.stringify({
-	"id": "urn:simple",
-	"@context": "https://www.w3.org/2022/wot/td/v1.1",
-	"title": "MyLampThing",
-	"description": "Valid TD copied from the spec's first example",
-	"securityDefinitions": {
-		"basic_sc": {
-			"scheme": "basic",
-			"in": "header"
-		}
-	},
-	"security": [
-		"basic_sc"
-	],
-	"properties": {
-		"status": {
-			"type": "string",
-			"forms": [
-				{
-					"href": "https://mylamp.example.com/status"
-				}
-			]
-		}
-	},
-	"actions": {
-		"toggle": {
-			"forms": [
-				{
-					"href": "https://mylamp.example.com/toggle"
-				}
-			]
-		}
-	},
-	"events": {
-		"overheating": {
-			"data": {
-				"type": "string"
-			},
-			"forms": [
-				{
-					"href": "https://mylamp.example.com/oh",
-					"subprotocol": "longpoll"
-				}
-			]
-		}
-	}
+    "id": "urn:version",
+    "@context":"https://www.w3.org/2022/wot/td/v1.1",
+    "title": "MyLampThing",
+    "description": "Valid TD demonstrating how to use version with additional fields",
+    "securityDefinitions": {
+        "basic_sc": {
+            "scheme": "basic",
+            "in": "header"
+        }
+    },
+    "security": ["basic_sc"],
+    "properties": {
+        "status": {
+            "type": "string",
+            "forms": [
+                {
+                    "href": "https://mylamp.example.com/status"
+                }
+            ]
+        }
+    },
+    "actions": {
+        "toggle": {
+            "forms": [
+                {
+                    "href": "https://mylamp.example.com/toggle"
+                }
+            ]
+        }
+    },
+    "events": {
+        "overheating": {
+            "data":{"type": "string"},
+            "forms": [
+                {
+                    "href": "https://mylamp.example.com/oh",
+                    "subprotocol": "longpoll"
+                }
+            ]
+        }
+    },
+    "version": {
+        "instance": "1.2.1",
+        "xyz:firmware": "0.9.1",
+        "xyz:hardware": "1.0"
+    }
 })
 
 tdValidator(simpleTD, console.log, {})
