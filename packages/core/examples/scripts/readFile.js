0d376b378255d4fc9a6044bb25ed25fb2a4f1585
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2023-03-11 01:15:15 +0100

    new examples

--- packages/core/examples/scripts/readFile.js
+++ packages/core/examples/scripts/readFile.js
@@ -2,9 +2,13 @@
 const tdValidator = require("../../index").tdValidator
 const fs = require("fs")
 
-const simpleTD = fs.readFileSync("../tds/valid/simple.json")
+const data = fs.readFileSync("../tds/valid/simple.json")
+const simpleTD = data.toString()
 
-/**
+
+console.log(simpleTD)
+
+/*
  * No logging and no additional checks
  */
 tdValidator(simpleTD, ()=>{}, {checkDefaults: false, checkJsonLd: false})
