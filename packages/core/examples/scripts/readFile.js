4cc11222a003c486650626d7ee15e601c9bf1081
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2023-04-05 15:26:35 +0200

    reverted the readFile

--- packages/core/examples/scripts/readFile.js
+++ packages/core/examples/scripts/readFile.js
@@ -1,58 +1,17 @@
 // Test utility to test index.js
 const tdValidator = require("../../index").tdValidator
-const fs = require('fs')
+const fs = require("fs")
 
-// Function to get current filenames in directory
+const simpleTD = fs.readFileSync("../tds/valid/simple.json")
 
-const fileNames = fs.readdirSync("../../../../examples/td/1-simple-default/")
-
-fileNames.forEach(file => {
-	const data = fs.readFileSync("../../../../examples/td/1-simple-default/"+file)
-	const TD = data.toString()
-	tdValidator(TD, ()=>{}, {checkDefaults: false, checkJsonLd: false})
-	.then( result => {
-		console.log("File: ", file)
-		console.log("OKAY")
-		console.log(result)
-		console.log("________________")
-	}, err => {
-		console.log("ERROR")
-		console.error(err)
-	})
+/**
+ * No logging and no additional checks
+ */
+tdValidator(simpleTD, ()=>{}, {checkDefaults: false, checkJsonLd: false})
+.then( result => {
+	console.log("OKAY")
+	console.log(result)
+}, err => {
+	console.log("ERROR")
+	console.error(err)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Test utility to test index.js
-// const tdValidator = require("../../index").tdValidator
-// const fs = require("fs")
-// const commander = require('commander')
-
-// const data = fs.readFileSync("../../../../examples/td/1-simple-default/basic-td.td.jsonld")
-// const simpleTD = data.toString()
-
-// /*
-//  * No logging and no additional checks
-//  */
-
-// tdValidator(simpleTD, ()=>{}, {checkDefaults: false, checkJsonLd: false})
-// .then( result => {
-// 	console.log("OKAY")
-// 	console.log(result)
-// }, err => {
-// 	console.log("ERROR")
-// 	console.error(err)
-// })
