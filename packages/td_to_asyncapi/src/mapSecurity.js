21094a04c4fbfa86f167399edc48b49c18314896
Monika Singh <monika.nic91@gmail.com>
2023-08-22 13:48:55 +0200

    Removing unused code

--- packages/td_to_asyncapi/src/mapSecurity.js
+++ packages/td_to_asyncapi/src/mapSecurity.js
@@ -13,7 +13,7 @@
  *  SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
  */
 
-module.exports = { mapSecurity, mapSecurityString, mapSecurityDefinitions, hasNoSec, mapFormSecurity };
+module.exports = { mapSecurity, mapSecurityString, mapSecurityDefinitions, mapFormSecurity };
 
 /**
  * Convert the TD security Definitions and Security to
