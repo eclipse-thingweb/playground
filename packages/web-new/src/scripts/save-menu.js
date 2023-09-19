9e3bee6da14c8fb97c61fd8171be79da7975ea2c
Sergio Eduardo Castro Ceballos <72516762+SergioCasCeb@users.noreply.github.com>
2023-09-19 15:41:00 +0200

    added license headers

--- packages/web-new/src/scripts/save-menu.js
+++ packages/web-new/src/scripts/save-menu.js
@@ -1,3 +1,18 @@
+/* 
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *  
+ *  See the NOTICE file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *  
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License v. 2.0 which is available at
+ *  http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ *  Document License (2015-05-13) which is available at
+ *  https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ *  
+ *  SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ */
+
 /**
  * @file The `save-menu.js` handle the main functionality for the save menu
  * such as generating a sharable link, allowing to open such link in a new playground
