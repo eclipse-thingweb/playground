89e45737d0b3fd52521253253a303261146e04ac
Bobur Khayitov <bobur.khayitov@siemens.com>
2026-04-08 20:18:50 +0200

    init

---
+++ packages/code-gen/types.ts
@@ -0,0 +1,36 @@
+export type TD = Record<AffordanceType, Record<string, Affordance>>;
+
+export const affordance_types = ["properties", "actions", "events"] as const;
+export type AffordanceType = (typeof affordance_types)[number];
+export interface Affordance {
+    forms: {
+        href: string;
+        op: Op | Op[];
+    }[];
+}
+
+export const operations = {
+    property: [
+        "readproperty",
+        "readallproperties",
+        "readmultiproperties",
+        "writeproperty",
+        "writeallproperties",
+        "writemultiproperties",
+        "observeproperty",
+        "observeallproperties",
+        "unobserveproperty",
+        "unobserveallproperties",
+    ],
+    action: ["invokeaction", "queryaction", "cancelaction", "queryallactions"],
+    event: ["subscribeevent", "unsubscribeevent", "subscribeallevents", "unsubscribeallevents"],
+} as const;
+export type Op = (typeof operations)[keyof typeof operations][number];
+
+export const supportedLanguages = ["javascript", "python", "java"] as const;
+export type SupportedLanguage = (typeof supportedLanguages)[number];
+export const supportedLibraries: Record<SupportedLanguage, string[]> = {
+    javascript: ["fetch", "axios"],
+    python: ["requests", "httpx"],
+    java: ["okhttp", "apache-httpclient"],
+};
